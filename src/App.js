a4d839f60d301179f7f71f55382556a9671918da
hongxiao <hongtao.xiao@unb.ca>
2019-04-09 10:46:18 -0300

    implement the static part of view

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ class App extends Component {
     };
 
     const view = model => {
+      let minutes = Math.floor(model.time / 60);
+      let seconds = model.time % 60;
+      let secondsFormatted = `${seconds < 10 ? "0" : ""}${seconds}`;
       return (
         <div>
-          <div>{model.time}</div>
+          <div>
+            {minutes}:{secondsFormatted}
+          </div>
+          <button onClick={console.log}>Button</button>
         </div>
       );
     };
