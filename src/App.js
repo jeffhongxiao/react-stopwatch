4b57f1d7694f44fb1eff8c59737d0fd68995bbf7
hongxiao <hongtao.xiao@unb.ca>
2019-04-09 10:59:16 -0300

    add onClick handler, which calls the update()

--- src/App.js
+++ src/App.js
@@ -4,19 +4,33 @@ import "./App.css";
 class App extends Component {
   render() {
     let model = {
+      running: false,
       time: 0
+    };
+
+    const update = (model, intent) => {
+      const updates = {
+        'STOP': (m) => {return Object.assign({}, m, {running: false})},
+        'START': (m) => {return Object.assign({}, m, {running: true})},
+      };
+      return updates[intent](model);
     };
 
     const view = model => {
       let minutes = Math.floor(model.time / 60);
       let seconds = model.time % 60;
       let secondsFormatted = `${seconds < 10 ? "0" : ""}${seconds}`;
+
+      let handler = event => {
+        model = update(model, model.running ? "STOP" : "START");
+      };
+
       return (
         <div>
           <div>
             {minutes}:{secondsFormatted}
           </div>
-          <button onClick={console.log}>Button</button>
+          <button onClick={handler}>{model.running ? 'Stop' : 'Start'}</button>
         </div>
       );
     };
